b4b6668e2421cb8ad01e8ff34a7ea4f15b14e9a2
Nastassia Fulconis <nastassia.fulconis@gmail.com>
2021-03-20 13:20:28 -0400

    card #64: created update report route

--- server/models/updateReport.js
+++ server/models/updateReport.js
@@ -1,5 +1,12 @@
 const pool = require('../db');
 
-exports.updateReport = (callback) => {
-  callback(null, 'hello update report!');
+exports.updateReport = (review_id, callback) => {
+  pool.query(`UPDATE reviews SET reported=true WHERE review_id=${review_id}`)
+    .then((data) => {
+      callback(null, data);
+    })
+    .catch((err) => {
+      console.error(`error updating report for review #${review_id}: `, err);
+      callback(err);
+    })
 }
