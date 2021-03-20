0e8b02e16fe57f1e985e321c61fc211edf07a23e
Nastassia Fulconis <nastassia.fulconis@gmail.com>
2021-03-20 13:18:30 -0400

    card #63: created update helpful route

--- server/models/updateHelpful.js
+++ server/models/updateHelpful.js
@@ -1,5 +1,12 @@
 const pool = require('../db');
 
-exports.updateHelpful = (callback) => {
-  callback(null, 'hello update helpful!');
+exports.updateHelpful = (review_id, callback) => {
+  pool.query(`UPDATE reviews SET helpfulness=helpfulness + 1 WHERE review_id=${review_id}`)
+    .then((data) => {
+      callback(null, data);
+    })
+    .catch((err) => {
+      console.error(`error updating helpful for review #${review_id}: `, err);
+      callback(err);
+    })
 }
