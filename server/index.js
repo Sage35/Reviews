c5feab91e379096241b6fe0f0a915bf7c2b1acb0
Nastassia Fulconis <nastassia.fulconis@gmail.com>
2021-03-22 11:38:40 -0400

    card #69: configured axios requests to point to server using localhost env var

--- server/index.js
+++ server/index.js
@@ -6,6 +6,13 @@ const router = require('./utils/router.js');
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8000');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  next();
+});
+
 app.get('/', (req, res) => {
   res.status(200).send('connected to server!')
 });
