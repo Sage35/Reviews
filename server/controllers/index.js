61c2f7da28ef8f067259714e970802777af73592
Nastassia Fulconis <nastassia.fulconis@gmail.com>
2021-03-24 15:20:45 -0400

    added a metric to capture query timing

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -12,84 +12,97 @@ client.socket.on('error', function(error) {
 });
 
 exports.getReviews = (req, res) => {
-  const start = new Date();
   client.increment('getReviews');
   const id = req.query.product_id || 17;
   const sort = req.query.sort || 'rating';
   const count = req.query.count || 5;
   const page = req.query.page || 1;
 
+  const dbStart = new Date();
   models.getReviews(id, sort, count, page, (err, data) => {
     if (err) {
       console.error('error getting reviews: ', err);
       res.sendStatus(500);
     } else {
+      const dbEnd = new Date() - dbStart;
+      client.timing('getReviews_db', dbEnd);
       res.status(200).send(data);
     }
-    const end = new Date() - start;
-    client.timing('getReviews_response', end);
+    const resEnd = new Date() - resStart;
+    client.timing('getReviews_response', resEnd);
   })
 };
 
 exports.getMeta = (req, res) => {
-  const start = new Date();
   client.increment('getMeta');
   const id = req.query.product_id || 17;
 
+  const dbStart = new Date();
   models.getMeta(id, (err, data) => {
     if (err) {
       console.error('error posting reviews: ', err);
       res.sendStatus(500);
     } else {
+      const dbEnd = new Date() - dbStart;
+      client.timing('getMeta_db', dbEnd);
       res.status(200).send(data);
     }
-    const end = new Date() - start;
-    client.timing('getMeta_response', end);
+    const resEnd = new Date() - resStart;
+    client.timing('getMeta_response', resEnd);
   })
 };
 
 exports.postReviews = (req, res) => {
-  const start = new Date();
   client.increment('postReview');
+
+  const dbStart = new Date();
   models.postReviews(req.body, (err, data) => {
     if (err) {
       console.error('error posting reviews: ', err);
       res.sendStatus(500);
     } else {
+      const dbEnd = new Date() - dbStart;
+      client.timing('postReviews_db', dbEnd);
       res.status(201).send(data);
     }
-    const end = new Date() - start;
-    client.timing('postReviews_response', end);
+    const resEnd = new Date() - resStart;
+    client.timing('postReviews_response', resEnd);
   })
 };
 
 exports.updateHelpful = (req, res) => {
-  const start = new Date();
   client.increment('updateHelpful');
+
+  const dbStart = new Date();
   models.updateHelpful(req.params.review_id, (err, data) => {
     if (err) {
       console.error('error updating reviews: ', err);
       res.sendStatus(500);
     } else {
+      const dbEnd = new Date() - dbStart;
+      client.timing('postReviews_db', dbEnd);
       res.status(201).send(data);
     }
-    const end = new Date() - start;
-    client.timing('updateHelpful_response', end);
+    const resEnd = new Date() - resStart;
+    client.timing('updateHelpful_response', resEnd);
   })
 };
 
 exports.updateReport = (req, res) => {
-  const start = new Date();
   client.increment('updateReport');
+
+  const dbStart = new Date();
   models.updateReport(req.params.review_id, (err, data) => {
     if (err) {
       console.error('error updating reviews: ', err);
       res.sendStatus(500);
     } else {
+      const dbEnd = new Date() - dbStart;
+      client.timing('postReviews_db', dbEnd);
       res.status(201).send(data);
     }
-    const end = new Date() - start;
-    client.timing('updateReport_response', end);
+    const resEnd = new Date() - resStart;
+    client.timing('updateReport_response', resEnd);
   })
 };
 
