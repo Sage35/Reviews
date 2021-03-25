e1005b8b0c4d45907d3801ba09fa816cf1aa7351
Nastassia Fulconis <nastassia.fulconis@gmail.com>
2021-03-25 16:04:45 -0400

    initialized redis cache for getMeta route

--- server/models/getMeta.js
+++ server/models/getMeta.js
@@ -1,69 +1,82 @@
 const pool = require('../db');
+const client = require('../redis.js');
 // const stats = require('../controllers');
 
 exports.getMeta = (id, callback) => {
-  const result = {
-    product_id: id
-  }
-  // const ratingStart = new Date();
-  pool.query(`SELECT rating, COUNT(*) FROM reviews where product=${id} and reported=false GROUP BY rating`)
-    .then(({rows}) => {
-      result.ratings = {
-        1: "0",
-        2: "0",
-        3: "0",
-        4: "0",
-        5: "0"
+  client.get(id, (err, data) => {
+    if (err) {
+      console.error(`error getting product #${id} from redis cache: ${err}`);
+    }
+    if (data !== null) {
+      callback(null, JSON.parse(data));
+      return;
+    } else {
+      const result = {
+        product_id: id
       }
-      for (let item of rows) {
-        result.ratings[item.rating] = item.count;
-      }
-      // const ratingEnd = new Date() - ratingStart;
-      // stats.client.timing('Meta_ratingQuery', ratingEnd);
-    })
-    .catch((err) => {
-      console.error(`error adding ratings for product #${id}: `, err);
-    })
-    .then(() => {
-      // const recommendStart = new Date();
-      pool.query(`SELECT recommend, COUNT(*) FROM reviews where product=${id} and reported=false GROUP BY recommend ORDER BY recommend`)
+      // const ratingStart = new Date();
+      pool.query(`SELECT rating, COUNT(*) FROM reviews where product=${id} and reported=false GROUP BY rating`)
         .then(({rows}) => {
-          result.recommended = {
-            false: 0,
-            true: 0
+          result.ratings = {
+            1: "0",
+            2: "0",
+            3: "0",
+            4: "0",
+            5: "0"
           }
           for (let item of rows) {
-            result.recommended[item.recommend] = item.count;
+            result.ratings[item.rating] = item.count;
           }
-          // const recommendEnd = new Date() - recommendStart;
-          // stats.client.timing('Meta_recommendQuery', recommendEnd);
+          // const ratingEnd = new Date() - ratingStart;
+          // stats.client.timing('Meta_ratingQuery', ratingEnd);
         })
         .catch((err) => {
-          console.error(`error adding recommended for product #${id}: `, err);
+          console.error(`error adding ratings for product #${id}: `, err);
         })
         .then(() => {
-          // const charStart = new Date();
-          pool.query(`SELECT chars.name, char_info.char_id, AVG(char_info.value) FROM chars FULL JOIN char_info ON chars.id=char_info.char_id WHERE chars.product_id=${id} GROUP BY chars.name, char_info.char_id`)
+          // const recommendStart = new Date();
+          pool.query(`SELECT recommend, COUNT(*) FROM reviews where product=${id} and reported=false GROUP BY recommend ORDER BY recommend`)
             .then(({rows}) => {
-              result.characteristics = {};
-              rows.map((char) => {
-                result.characteristics[char.name] = {
-                  id: char.char_id,
-                  value: char.avg
-                }
-                return char;
-              })
-              // const charEnd = new Date() - charStart;
-              // stats.client.timing('Meta_charsQuery', charEnd);
-              callback(null, result);
+              result.recommended = {
+                false: 0,
+                true: 0
+              }
+              for (let item of rows) {
+                result.recommended[item.recommend] = item.count;
+              }
+              // const recommendEnd = new Date() - recommendStart;
+              // stats.client.timing('Meta_recommendQuery', recommendEnd);
             })
             .catch((err) => {
-              console.error(`error adding characteristics for product #${id}: `, err);
+              console.error(`error adding recommended for product #${id}: `, err);
+            })
+            .then(() => {
+              // const charStart = new Date();
+              pool.query(`SELECT chars.name, char_info.char_id, AVG(char_info.value) FROM chars FULL JOIN char_info ON chars.id=char_info.char_id WHERE chars.product_id=${id} GROUP BY chars.name, char_info.char_id`)
+                .then(({rows}) => {
+                  result.characteristics = {};
+                  rows.map((char) => {
+                    result.characteristics[char.name] = {
+                      id: char.char_id,
+                      value: char.avg
+                    }
+                    return char;
+                  })
+                  // const charEnd = new Date() - charStart;
+                  // stats.client.timing('Meta_charsQuery', charEnd);
+
+                  client.setex(id, 60, JSON.stringify(result));
+                  callback(null, result);
+                })
+                .catch((err) => {
+                  console.error(`error adding characteristics for product #${id}: `, err);
+                })
             })
         })
-    })
-    .catch((err) => {
-      console.error(`error fetching meta characteristics for product #${id}: `, err);
-      callback(err);
-    })
+        .catch((err) => {
+          console.error(`error fetching meta characteristics for product #${id}: `, err);
+          callback(err);
+        })
+    }
+  })
 }
