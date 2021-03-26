29eb0f74738f3a96836e61c556026556515a5182
Nastassia Fulconis <nastassia.fulconis@gmail.com>
2021-03-26 19:39:04 -0400

    modularized getReviews code

--- server/models/getReviews.js
+++ server/models/getReviews.js
@@ -2,13 +2,7 @@ const pool = require('../db');
 const client = require('../redis.js');
 
 exports.getReviews = (id, sort, count, page, callback) => {
-  if (sort === 'relevant') {
-    sort = 'rating'
-  } else if (sort === 'helpful') {
-    sort = 'helpfulness'
-  } else if (sort === 'newest') {
-    sort = 'date'
-  }
+  sort = changeSort(sort);
 
   client.get(`${id}_${sort}_${count}_${page}`, (err, data) => {
     if (err) {
@@ -18,42 +12,64 @@ exports.getReviews = (id, sort, count, page, callback) => {
       callback(null, JSON.parse(data));
       return;
     } else {
-      const result = {
+      const obj = {
         product: id,
         page,
         count
       };
 
-      pool.query(`SELECT * FROM reviews WHERE reviews.product=${id} AND reviews.reported=false ORDER BY reviews.${sort} DESC LIMIT ${count} OFFSET ${count * (page - 1)}`)
-        .then((data) => {
-          result.results = data.rows
-          return result.results;
+      getReviews(id, sort, count, page, obj)
+        .then((resultObj) => {
+          return getPhotos(id, resultObj);
         })
-        .then((reviews) => {
-          const photos = pool.query(`SELECT photos.review_id, photos.id, photos.url FROM photos LEFT JOIN (SELECT review_id, product, reported FROM reviews) r USING (review_id) WHERE r.product=${id} AND r.reported=false`)
-            .then(({rows}) => {
-              reviews.map((review) => {
-                review.photos = [];
-                for (let photo of rows) {
-                  if (photo.review_id === review.review_id) {
-                    review.photos.push({
-                      id: photo.id,
-                      url: photo.url
-                    })
-                  }
-                }
-                return review;
-              })
-            })
-            .then(() => {
-              client.setex(`${id}_${sort}_${count}_${page}`, 5, JSON.stringify(result));
-              callback(null, result);
-            })
-            .catch((err) => {
-              console.error(`error getting reviews for product #${id}: `, err);
-              callback(err);
-            });
+        .then((result) => {
+          client.setex(`${id}_${sort}_${count}_${page}`, 5, JSON.stringify(result));
+          callback(null, result);
         })
+        .catch((err) => {
+          console.error(`error getting reviews for product #${id}: `, err);
+          callback(err);
+        });
     }
   })
+};
+
+var changeSort = (sort) => {
+  if (sort === 'relevant') {
+    sort = 'rating'
+  } else if (sort === 'helpful') {
+    sort = 'helpfulness'
+  } else if (sort === 'newest') {
+    sort = 'date'
+  }
+
+  return sort;
+};
+
+var getReviews = (id, sort, count, page, result) => {
+  return pool.query(`SELECT * FROM reviews WHERE reviews.product=${id} AND reviews.reported=false ORDER BY reviews.${sort} DESC LIMIT ${count} OFFSET ${count * (page - 1)}`)
+    .then((data) => {
+      result.results = data.rows
+      return result;
+    })
+};
+
+var getPhotos = (id, result) => {
+  const reviews = result.results
+  return pool.query(`SELECT photos.review_id, photos.id, photos.url FROM photos LEFT JOIN (SELECT review_id, product, reported FROM reviews) r USING (review_id) WHERE r.product=${id} AND r.reported=false`)
+    .then(({rows}) => {
+      reviews.map((review) => {
+        review.photos = [];
+        for (let photo of rows) {
+          if (photo.review_id === review.review_id) {
+            review.photos.push({
+              id: photo.id,
+              url: photo.url
+            })
+          }
+        }
+        return review;
+      })
+      return result;
+    })
 };
